3612b5a6378322995dad99e23d4ff097a4e5a6d5
İbrahim Baymaz <20200808006@ogr.akdeniz.edu.tr>
2023-07-24 11:38:04 +0300

    Colors has added.

---
+++ lib/constants/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class MyColors {
+  static const Color greyVideo = Color(0xFF333333);
+  static const Color blackMain = Color(0xFF0F0F0F);
+  static const Color greyButton = Color(0xFF272727);
+  static const Color blueTextButton = Color(0xFF5CA4F8);
+}
