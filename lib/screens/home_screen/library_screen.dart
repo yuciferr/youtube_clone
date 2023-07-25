dee4f83b75328417175c25ebcb982a115e7a3a2c
Bedirhan Tonğ <20200808033@ogr.akdeniz.edu.tr>
2023-07-25 15:20:52 +0300

    new feature : playlist of user that he/she have been in contact are implemented into library screen

--- lib/screens/home_screen/library_screen.dart
+++ lib/screens/home_screen/library_screen.dart
@@ -49,40 +49,16 @@ class _LibraryScreenState extends State<LibraryScreen> {
               child: Row(
                 children: [
                   buildHistoryVideos('What is Flutter?', '12 watched'),
-                  buildHistoryVideos('What is Flutter?', '12 watched'),
-                  buildHistoryVideos('What is Flutter?', '12 watched'),
-                  buildHistoryVideos('What is Flutter?', '12 watched'),
-                  buildHistoryVideos('What is Flutter?', '12 watched'),
-                  buildHistoryVideos('What is Flutter?', '12 watched'),
+                  buildHistoryVideos('How to use Chat Gpt?', '4M watched'),
+                  buildHistoryVideos('How to learn programming?', '12 watched'),
+                  buildHistoryVideos('What is Java?', '12 watched'),
+                  buildHistoryVideos('What is .Net?', '12 watched'),
+                  buildHistoryVideos('What is Asp .Net Core?', '12 watched'),
                 ],
               ),
             ),
-            SizedBox(
+            const SizedBox(
               height: 30,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                TextButton.icon(
-                  onPressed: () {},
-                  icon: const Icon(
-                    Icons.playlist_play_rounded,
-                    color: Colors.white,
-                    size: 30.0,
-                  ),
-                  label: const Text(
-                    'Play Lists',
-                    style: TextStyle(color: Colors.white, fontSize: 17.0),
-                  ),
-                ),
-                TextButton(
-                  onPressed: () {},
-                  child: const Text(
-                    'View all',
-                    style: TextStyle(color: Colors.blue, fontSize: 17.0),
-                  ),
-                ),
-              ],
             ),
             Container(
               decoration: const BoxDecoration(
@@ -91,19 +67,70 @@ class _LibraryScreenState extends State<LibraryScreen> {
                 ),
               ),
               child: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 28.0),
-                child: SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Row(
-                    children: [
-                      buildHistoryVideos('What is Flutter?', '12 watched'),
-                      buildHistoryVideos('What is Flutter?', '12 watched'),
-                      buildHistoryVideos('What is Flutter?', '12 watched'),
-                      buildHistoryVideos('What is Flutter?', '12 watched'),
-                      buildHistoryVideos('What is Flutter?', '12 watched'),
-                      buildHistoryVideos('What is Flutter?', '12 watched'),
-                    ],
-                  ),
+                padding: const EdgeInsets.symmetric(vertical: 17.0),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        TextButton.icon(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.playlist_play_rounded,
+                            color: Colors.white,
+                            size: 30.0,
+                          ),
+                          label: const Text(
+                            'Play Lists',
+                            style:
+                                TextStyle(color: Colors.white, fontSize: 17.0),
+                          ),
+                        ),
+                        TextButton(
+                          onPressed: () {},
+                          child: const Text(
+                            'View all',
+                            style:
+                                TextStyle(color: Colors.blue, fontSize: 17.0),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        children: [
+                          buildPlayList(
+                            'How to use Chat Gpt?',
+                            '4M watched',
+                            const Icon(Icons.playlist_play_outlined,
+                                color: Colors.white),
+                          ),
+                          buildPlayList(
+                            'How to use Chat Gpt?',
+                            '4M watched',
+                            const Icon(Icons.thumb_up_alt, color: Colors.white),
+                          ),
+                          buildPlayList(
+                            'How to learn programming?',
+                            '12 watched',
+                            const Icon(Icons.thumb_down_alt,
+                                color: Colors.white),
+                          ),
+                          buildPlayList(
+                            'What is Asp .Net Core?',
+                            '12 watched',
+                            const Icon(Icons.thumb_up_alt, color: Colors.white),
+                          ),
+                          buildPlayList(
+                            'What is .Net?',
+                            '24 watched',
+                            const Icon(Icons.thumb_up_alt, color: Colors.white),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ),
@@ -141,13 +168,67 @@ class _LibraryScreenState extends State<LibraryScreen> {
             ),
           ],
         ),
-        Text(
-          title,
-          style: const TextStyle(color: Colors.white),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              title,
+              style: const TextStyle(color: Colors.white),
+            ),
+            Text(
+              viewedNum,
+              style: const TextStyle(color: Colors.white60),
+            ),
+          ],
         ),
-        Text(
-          viewedNum,
-          style: const TextStyle(color: Colors.white60),
+      ],
+    );
+  }
+
+  Column buildPlayList(String title, String viewedNum, Icon icon) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Stack(
+          children: [
+            Container(
+              height: 70,
+              width: 120,
+              margin: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.grey.withOpacity(0.1),
+              ),
+            ),
+            Positioned(
+              bottom: 10,
+              right: 10,
+              top: 10,
+              left: 10,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  icon,
+                  Text('268'),
+                ],
+              ),
+            ),
+          ],
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              title,
+              style: const TextStyle(color: Colors.white),
+            ),
+            Text(
+              viewedNum,
+              style: const TextStyle(color: Colors.white60),
+            ),
+          ],
         ),
       ],
     );
