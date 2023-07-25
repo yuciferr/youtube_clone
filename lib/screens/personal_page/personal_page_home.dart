5c1ef55191817b72125be0d95b79e3d7400f2e15
İbrahim Baymaz <20200808006@ogr.akdeniz.edu.tr>
2023-07-25 13:18:33 +0300

    VideoCardList has added to TabBarVideos.

--- lib/screens/personal_page/personal_page_home.dart
+++ lib/screens/personal_page/personal_page_home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:youtube_clone/screens/personal_page/channel_videos_page.dart';
 
 class PersonalPageHome extends StatefulWidget {
   const PersonalPageHome({super.key});
@@ -89,9 +90,7 @@ class _PersonalPageHomeState extends State<PersonalPageHome> {
                       Center(
                         child: Text('HOME'),
                       ),
-                      Center(
-                        child: Text('VIDEOS'),
-                      ),
+                      ChannelVideosPage(),
                       Center(
                         child: Text('SHORTS'),
                       ),
