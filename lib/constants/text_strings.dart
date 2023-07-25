b67bba957f4de22b2cd7a57d71effd892baae9b2
Bedirhan Tonğ <20200808033@ogr.akdeniz.edu.tr>
2023-07-25 13:01:34 +0300

    other users posts are showed on main screen with dummy data

---
+++ lib/constants/text_strings.dart
@@ -0,0 +1,22 @@
+class MyStrings {
+  // post texts
+  static const String postStringsSerhanDiaryPostTitle =
+      'Krakow diaries : How did I get accepted? I answered your questions';
+  static const String postStringsSerhanDiaryPostDescription =
+      'Serhan Baymaz 3M views 2 days ago';
+
+  static const String postStringsOnurFrogPostTitle =
+      'Drive your mates crazy by throwing frog reels';
+  static const String postStringsOnurFrogPostDescription =
+      'Onur Cetin 40M views 6 days ago';
+
+  static const String postStringsYusufJobPostTitle =
+      'I crossed the Sıraat bridge in Kultur neighbourhood twice a day it (very risky)';
+  static const String postStringsYusufJobPostDescription =
+      'Yusuf Celik 3M views 20 years ago';
+
+  static const String postStringsBedirhanPostTitle =
+      'We finally got the gang together and had a Popeyes menu';
+  static const String postStringsBedirhanPostDescription =
+      'Bedirhan Tong 3M 1 day ago';
+}
--- lib/screens/home_screen/home_screen.dart
+++ lib/screens/home_screen/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:youtube_clone/constants/colors.dart';
 import 'package:youtube_clone/constants/image_strings.dart';
+import 'package:youtube_clone/constants/text_strings.dart';
 import 'package:youtube_clone/screens/home_screen/subscriptions_screen.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -15,42 +16,97 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: buildAppBar(),
-      body: Column(
-        children: [
-          SingleChildScrollView(
-            child: Row(
-              children: [
-                Card(
-                  shape: const RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(10),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          children: [
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  Card(
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(10),
+                      ),
+                    ),
+                    color: MyColors.greyVideo,
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.explore,
+                        color: Colors.white,
+                      ),
                     ),
                   ),
-                  color: MyColors.greyVideo,
-                  child: IconButton(
-                    onPressed: () {},
-                    icon: const Icon(
-                      Icons.explore,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-                Card(
-                  shape: const RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(10),
-                    ),
-                  ),
-                  color: MyColors.greyVideo,
-                  child: TextButton(
-                    onPressed: () {},
-                    child: Text(''),
-                  ),
-                ),
-              ],
+                  buildCardPagesForHome('All'),
+                  buildCardPagesForHome('Mixes'),
+                  buildCardPagesForHome('Music'),
+                  buildCardPagesForHome('Live'),
+                  buildCardPagesForHome('Game'),
+                  buildCardPagesForHome('Volleyball'),
+                  buildCardPagesForHome('Soccer'),
+                  buildCardPagesForHome('Rp'),
+                  buildCardPagesForHome('Last uploads'),
+                  buildCardPagesForHome('Suggest a feedback'),
+                ],
+              ),
             ),
-          ),
-        ],
+            const SizedBox(height: 20),
+            SizedBox(
+              child: Column(
+                children: [
+                  BuildChannelSinglePost(
+                    postImage: MyImageStrings.dummyDataChannelYusufImage,
+                    channelImage: MyImageStrings.dummyDataChannelYusufImage,
+                    postDescription: MyStrings.postStringsYusufJobPostTitle,
+                    channelDescription:
+                        MyStrings.postStringsYusufJobPostDescription,
+                  ),
+                  BuildChannelSinglePost(
+                    postImage: MyImageStrings.dummyDataChannelSerhanImage,
+                    channelImage: MyImageStrings.dummyDataChannelSerhanImage,
+                    postDescription: MyStrings.postStringsSerhanDiaryPostTitle,
+                    channelDescription:
+                        MyStrings.postStringsSerhanDiaryPostDescription,
+                  ),
+                  BuildChannelSinglePost(
+                    postImage: MyImageStrings.dummyDataChannelOnurImage,
+                    channelImage: MyImageStrings.dummyDataChannelOnurImage,
+                    postDescription: MyStrings.postStringsOnurFrogPostTitle,
+                    channelDescription:
+                        MyStrings.postStringsOnurFrogPostDescription,
+                  ),
+                  BuildChannelSinglePost(
+                    postImage: MyImageStrings.dummyDataChannelBedirhanImage,
+                    channelImage: MyImageStrings.dummyDataChannelBedirhanImage,
+                    postDescription: MyStrings.postStringsBedirhanPostTitle,
+                    channelDescription:
+                        MyStrings.postStringsBedirhanPostDescription,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Card buildCardPagesForHome(String title) {
+    return Card(
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(
+          Radius.circular(10),
+        ),
+      ),
+      color: MyColors.greyVideo,
+      child: TextButton(
+        onPressed: () {},
+        child: Text(
+          title,
+          style: const TextStyle(color: Colors.white),
+        ),
       ),
     );
   }
@@ -110,6 +166,99 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ),
       ],
+    );
+  }
+}
+
+class BuildChannelSinglePost extends StatelessWidget {
+  const BuildChannelSinglePost(
+      {super.key,
+      required this.postImage,
+      required this.channelImage,
+      required this.postDescription,
+      required this.channelDescription});
+
+  final String postImage;
+  final String channelImage;
+  final String postDescription;
+  final String channelDescription;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      child: Column(
+        children: [
+          Stack(
+            children: [
+              TextButton(
+                onPressed: () {},
+                child: SizedBox(
+                  width: 300,
+                  height: 200,
+                  child: Image.asset(
+                    postImage, // Resmi tamamen kaplayacak şekilde düzenledik
+                  ),
+                ),
+              ),
+              Positioned(
+                bottom: 0, // Align at the bottom
+                right: 0, // Align at the right
+                child: Container(
+                  color: MyColors.greyButton,
+                  // padding: const EdgeInsets.all(
+                  //     8), // Adding some padding around the text
+                  child: TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      '10:00',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              TextButton(
+                onPressed: () {
+                  // User Profile gidecek
+                },
+                child: CircleAvatar(
+                    backgroundImage: AssetImage(channelImage), radius: 26),
+              ),
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      postDescription,
+                      style: const TextStyle(color: Colors.white, fontSize: 17),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 8.0),
+                      child: Text(
+                        channelDescription,
+                        style: const TextStyle(
+                            color: Colors.white70, fontSize: 14),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.more_vert_sharp,
+                ),
+                color: Colors.white,
+              )
+            ],
+          )
+        ],
+      ),
     );
   }
 }
