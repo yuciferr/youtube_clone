00114ba77516fe19e01d1127a1994b2babfef1a8
Onur Cetin <onucetin003@gmail.com>
2023-07-25 13:40:22 +0300

    VideoPlayerPage2

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 
 import 'package:flutter/material.dart';
-import 'package:youtube_clone/video_list_page.dart';
+import 'package:youtube_clone/screens/video_list_page/video_list_page.dart';
 
 void main() {
   runApp(MaterialApp(
